82a37d1e1be8f91d3b1538fe326ee5c5020ae1d9
Brett Witherspoon <spoonb@cdspooner.com>
2018-11-24 11:32:13 -0600

    testbench: use stationary deterministic rewards

--- sim/testbench.sv
+++ sim/testbench.sv
@@ -21,24 +21,38 @@ module bandit_test;
   logic [7:0] action_data;
   logic action_ready = 0;
 
-  bandit bandit(.*);
+  bit [7:0] action;
+  bit signed [7:0] rewards [0:255];
 
-  initial begin
-    for (int i = 0; i < 256; i++) bandit.action_value_table[i] = 0;
-    dump_setup;
-    sync_reset;
+  bandit dut(.*);
+
+  task test_cases(int count);
+  begin
     @(negedge clock) action_ready = 1;
-    repeat (10) begin
+    repeat (count) begin
       wait (action_valid == 1);
+      action = action_data;
       wait (action_valid == 0);
       @(negedge clock) begin
         reward_valid = 1;
-        reward_data = $random;
+        reward_data = rewards[action];
       end
       wait (reward_ready == 1);
       @(posedge clock) #1 reward_valid = 0;
     end
-    @(negedge clock) $finish(0);
+  end
+  endtask : test_cases
+
+  initial begin
+    // Optimistic initial values to encourage exploration
+    for (int i = 0; i < $size(dut.action_value_table[i]); i++) dut.action_value_table[i] = 5;
+    // Reward only a few actions
+    for (int i = 0; i < $size(rewards); i++) rewards[i] = 0;
+    rewards[64] = 3;
+    dump_setup;
+    sync_reset;
+    test_cases(100);
+    @(negedge clock) $finish;
   end
 
 endmodule // BanditTest
