26981122d848e5c73411f6fcd083d5cb9baf82d2
Brett Witherspoon <spoonb@cdspooner.com>
2018-11-24 18:09:03 -0600

    bandit: use LFSR as address counter

--- syn/bandit.sv
+++ syn/bandit.sv
@@ -3,7 +3,10 @@
 `timescale 1ns / 1ps
 `default_nettype none
 
-module bandit (
+module bandit #(
+  parameter SEED = 8'hff,
+  parameter TAPS = 8'hb1
+)(
   input logic clock,
   input logic reset,
 
@@ -16,16 +19,20 @@ module bandit (
   input logic action_ready
 );
 
-  logic signed [15:0] action_value_table [0:255];
-  logic [7:0] action_value_index = 0;
-  logic signed [15:0] action_value = 0;
-  logic [7:0] action_index = 0;
-
-  // TODO Use enumuration when supported by yosys
+  // State register
+  // TODO Use enumuration for state when supported by yosys
   localparam DECIDING = 2'b00;
   localparam ACTUATING = 2'b01;
   localparam OBSERVING = 2'b10;
   logic [1:0] state = DECIDING;
+
+  // Registers and memory for action-value functions
+  logic signed [15:0] action_value_table [0:255];
+  logic [7:0] action_value_index = SEED;
+  logic [7:0] action_value_count = 0;
+
+  logic signed [15:0] action_value = 0;
+  logic [7:0] action_index = 0;
 
   // Mealy finite-state machine
   always_ff @(posedge clock) begin
@@ -34,7 +41,7 @@ module bandit (
     end else begin
       case (state)
         DECIDING:
-          if (&action_value_index) state <= ACTUATING;
+          if (&action_value_count) state <= ACTUATING; // FIXME 255 LFSR states
         ACTUATING:
           if (action_valid & action_ready) state <= OBSERVING;
         OBSERVING:
@@ -45,16 +52,26 @@ module bandit (
     end
   end
 
+  // Fibonacci LFSR for pseudorandom actions (default: x^8 + x^6 + x^5 + x^4 + 1)
+  always_ff @(posedge clock) begin
+    if (reset) begin
+      action_value_index <= SEED;
+    end else begin
+      action_value_index <= action_value_index << 1;
+      action_value_index[0] <= ^(action_value_index & TAPS);
+    end
+  end
+
   // A_n = argmax Q_n(a)
   always_ff @(posedge clock) begin
     if (reset) begin
-      action_value_index <= 0;
+      action_value_count <= 0;
     end else if (state == DECIDING) begin
       if (action_value_table[action_value_index] > action_value) begin
         action_value <= action_value_table[action_value_index];
         action_index <= action_value_index;
       end
-      action_value_index <= action_value_index + 1;
+      action_value_count <= action_value_count + 1;
     end
   end
 
