fe73ae4cbdc14f8b84a5255a35731be22880cfc9
Brett Witherspoon <spoonb@cdspooner.com>
2018-11-25 18:25:42 -0600

    sim: add level argument to dump

--- sim/dump.svh
+++ sim/dump.svh
@@ -3,11 +3,11 @@
 `ifndef DUMP_INCLUDED
 `define DUMP_INCLUDED
 
-task dump_setup;
+task dump_setup(int levels = 1);
   string file;
   if ($value$plusargs("dump=%s", file)) begin
     $dumpfile(file);
-    $dumpvars(1);
+    $dumpvars(levels);
   end
 endtask : dump_setup
 
